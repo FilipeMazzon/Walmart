c1eed7ce765026a8c6bc17e646d49c66807521d8
Filipe Mazzon <filipemazzon@gec.inatel.br>
2017-10-15 21:03:40 -0200

    Projeto Atualizado

--- app/controllers/admin/edit/cliente.js
+++ app/controllers/admin/edit/cliente.js
@@ -1,6 +1,15 @@
 module.exports.editar_cliente = function(application,req,res){
-    res.render("admin/edit/cliente");
-}
-module.exports.cliente_editar = function(application,req,res){
+    var connection = application.config.dbConnection();
+    var clientesDAO = new application.app.models.ClienteDAO(connection);
+    clientesDAO.getClientes(function (error,result) {
+        res.render("admin/edit/cliente",{validacao : {}, cliente:result});
+    });
 
-}
+};
+module.exports.cliente_editar = function(application,req,res){
+    var connection = application.config.dbConnection();
+    var clientesDAO = new application.app.models.ClienteDAO(connection);
+    clientesDAO.getClientes(function (error,result) {
+        res.render("admin/edit/cliente",{validacao : {}, cliente:result});
+    });
+};
--- app/routes/admin/edit/cliente.js
+++ app/routes/admin/edit/cliente.js
@@ -2,7 +2,7 @@ module.exports = function (application){
 	application.get('/editar_cliente',function(req,res){
 		application.app.controllers.admin.edit.cliente.editar_cliente(application,req,res);
 	});
-	application.post('/editar/cliente',function(req,res){
+    application.post('/editar_cliente', function (req, res) {
 		application.app.controllers.admin.edit.cliente.cliente_editar(application,req,res);
 	});
-}
+};
