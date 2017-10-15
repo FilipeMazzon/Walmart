898edd03d62ae5964bd41bc1f31739048c6353b9
Filipe Mazzon Ribeiro <filipemazzon@gec.inatel.br>
2017-10-15 20:01:44 -0200

    Projeto Atualizado

--- app/routes/admin/delete/item.js
+++ app/routes/admin/delete/item.js
@@ -1,9 +1,9 @@
-module.exports = function (application){
-	
-	application.get('/deletar_item',function(req,res){
-		application.app.controllers.admin.delete.item.deletar_item(application,req,res);
-	});
-	application.post('/deletar_item',function(req,res){
-		application.app.controllers.admin.delete.item.item_deletar(application,req,res);
-	});
-}
+module.exports = function (application) {
+
+    application.get('/deletar_item', function (req, res) {
+        application.app.controllers.admin.delete.item.deletar_item(application, req, res);
+    });
+    application.post('/deletar_item', function (req, res) {
+        application.app.controllers.admin.delete.item.item_deletar(application, req, res);
+    });
+};
